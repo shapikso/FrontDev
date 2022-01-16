ecc3e8250c1b2823ee37ae72025825eecfa3ca95
shapikso <79461623+shapikso@users.noreply.github.com>
2022-01-16 17:18:52 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlay } from '@fortawesome/free-solid-svg-icons'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="app">
+      
+      <div class="song-info">
+        <img
+        class="albumImg"
+          src="https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg"
+          alt=""
+        />
+        <div>
+          <h2 class="name">Quran</h2>
+          <h3 class="song">Hakim Omari</h3>
+        </div>
+      </div>
+      <div class="player-control">
+        
+        
+        <i class="fas fa-play" id="play-pause"><FontAwesomeIcon icon={faPlay} /></i>
+        <i class="fas fa-forward" id="forward"></i>
+
+      </div>
+
     </div>
   );
 }
