7b83e30b3fb7ad2801fc29bba9ae0a4086093503
shapikso <79461623+shapikso@users.noreply.github.com>
2022-01-29 20:48:11 +0200

    done template

--- src/App.js
+++ src/App.js
@@ -1,33 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPlay } from '@fortawesome/free-solid-svg-icons'
 
-function App() {
-  return (
-    <div class="app">
-      
-      <div class="song-info">
-        <img
-        class="albumImg"
-          src="https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg"
-          alt=""
-        />
-        <div>
-          <h2 class="name">Quran</h2>
-          <h3 class="song">Hakim Omari</h3>
-        </div>
-      </div>
-      <div class="player-control">
-        
-        
-        <i class="fas fa-play" id="play-pause"><FontAwesomeIcon icon={faPlay} /></i>
-        <i class="fas fa-forward" id="forward"></i>
+import React, {Component} from 'react';
 
-      </div>
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
 
-    </div>
-  );
+        };
+    }
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
 }
 
 export default App;
