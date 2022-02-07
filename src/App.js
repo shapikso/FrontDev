07c2260f88c3df33e3b0e9c36269d8643c2bc613
shapikso <79461623+shapikso@users.noreply.github.com>
2022-02-07 19:07:14 +0200

    done hw

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
 import React from "react";
 import './App.scss';
 
 const App = () => {
     const canvasRef = useRef(null);
     const requestIdRef = useRef(null);
+    let timerId = useRef();
     const ballRef = useRef([
-        { x: 50, y: 50, vx: 4, vy: 4, radius: 10, color: "#444"},
-        { x: 150, y: 150, vx: -4, vy: 4, radius: 5, color: "#1ec5c5"},
-        { x: 500, y: 500, vx: 4, vy: -4, radius: 20, color: "#40bd18"}
+        { x: 50, y: 50, vx: 2, vy: 4, radius: 10, color: "#b29090"},
+        { x: 150, y: 150, vx: -4, vy: -1, radius: 40, color: "#1ec5c5"},
+        { x: 50, y: 500, vx: 2, vy: -4, radius: 20, color: "#40bd18"},
+        { x: 10, y: 10, vx: 4, vy: -4, radius: 10, color: "#be8b8b"},
+        { x: 100, y: 10, vx: 4, vy: 5, radius: 50, color: "#1ec5c5"},
+        { x: 400, y: 40, vx: -4, vy: -4, radius: 20, color: "#40bd18"},
+        { x: 500, y: 0, vx: 2, vy: -1, radius: 15, color: "#b29090"},
+        { x: 66, y: 24, vx: -6, vy: -1, radius: 40, color: "#1ec5c5"},
+        { x: 250, y: 34, vx: 7, vy: -4, radius: 20, color: "#40bd18"},
+        { x: 10, y: 10, vx: -3, vy: -7, radius: 10, color: "#be8b8b"},
+        { x: 100, y: 10, vx: 1, vy: 2, radius: 50, color: "#1ec5c5"},
+        { x: 400, y: 40, vx: -2, vy: 0, radius: 20, color: "#40bd18"},
+
     ]);
+    const [isFiberAnimation, setIsFiberAnimation ] = useState(true);
     const size = { width: 800, height: 600 };
 
 
@@ -20,12 +32,17 @@ const App = () => {
         ctx.arc(x, y, radius, 0, Math.PI * 2);
         ctx.fillStyle = color;
         ctx.fill();
+        ctx.textAlign = 'center';
+        ctx.font = `${radius}px Arial`;
+        ctx.fillStyle = 'black';
+        ctx.textBaseline = 'middle';
+        const date = new Date();
+        ctx.fillText(date.getSeconds().toString(), x, y + radius/30);
         ctx.closePath();
         ctx.restore();
 
     };
-
-
+    
     const updateBall = (ball) => {
         ball.x += ball.vx;
         ball.y += ball.vy;
@@ -58,19 +75,33 @@ const App = () => {
     };
 
     const tick = () => {
-        if (!canvasRef.current) return;
-        renderFrame();
-        requestIdRef.current = requestAnimationFrame(tick);
+        if(isFiberAnimation){
+            renderFrame();
+            requestIdRef.current = requestAnimationFrame(tick);
+        } else {
+            timerId = setInterval(() => {renderFrame();}, 1000/30);
+        }
     };
 
     useEffect(() => {
-        requestIdRef.current = requestAnimationFrame(tick);
-        return () => {
-            cancelAnimationFrame(requestIdRef.current);
-        };
-    }, []);
+        clearInterval(timerId);
+        tick();
+    }, [isFiberAnimation]);
 
-    return <canvas {...size} ref={canvasRef} />;
+    const toggleAnimation = () => {
+        clearInterval(timerId);
+        cancelAnimationFrame(requestIdRef.current);
+        setIsFiberAnimation(!isFiberAnimation);
+    };
+    return (
+        <div>
+            <canvas {...size} ref={canvasRef} />
+            <div className="buttonWrapper">
+                <button onClick={toggleAnimation}> Change Animation </button>
+            </div>
+
+        </div>
+    );
 };
 
 export default App;
