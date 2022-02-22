eb1a5a661f05a2e4ba6d4600f52037233589ec72
shapikso <79461623+shapikso@users.noreply.github.com>
2022-02-22 11:20:48 +0200

    done hw

--- src/App.js
+++ src/App.js
@@ -1,34 +1,22 @@
 import React from 'react';
 import Form from './components/Form/Form';
 import List from './components/List/List';
-import {StThemeWrapper, StWrapper,} from "./styles";
+import {StWrapper} from "./styles";
 import Notification from './components/Notification/Notification';
 import {selectTheme} from './store/theme/selectors';
-import {useDispatch, useSelector} from "react-redux";
-import {switchToDrak, switchToLight} from "./store/theme/action";
+import {useSelector} from "react-redux";
 import {GlobalStyle} from "./GlobalStyles";
-import {StButton} from "./components/commonStyles/commonStyles";
+import Theme from "./components/Theme/Theme";
 
 const App = () =>{
-    const dispatch = useDispatch();
     const theme = useSelector(selectTheme);
-    const onclickHandler = () => {
-        if ( theme.primary === '#000') {
-            dispatch(switchToLight());
-        } else  {
-            dispatch(switchToDrak());
-        }
-    };
-
     return (
         <>
             <GlobalStyle theme={theme}/>
             <Notification/>
+            <Theme/>
             <StWrapper theme={theme} >
                 <h1>Todo App</h1>
-                <StThemeWrapper>
-                    <StButton width="200px" fontSize="20px" theme={theme} onClick={onclickHandler}>Change Theme</StButton>
-                </StThemeWrapper>
                 <Form />
                 <List/>
             </StWrapper>
--- src/store/theme/reducer.js
+++ src/store/theme/reducer.js
@@ -1,21 +1,6 @@
 import {SWITCH_LIGHT_THEME, SWITCH_DARK_THEME} from "./actionTypes";
+import {darkTheme, lightTheme} from "../../constants/themes";
 
-const darkTheme = {
-    primary: '#000',
-    secondary:'#fff',
-    backgroundFirst:'#285c50',
-    backgroundSecond:'#111d36',
-    buttonColor: '#8e49e8'
-};
-const lightTheme = {
-    primary: '#fff',
-    secondary:'#000',
-    backgroundFirst:'#68Eacc',
-    backgroundSecond:'#497BE8',
-    buttonColor: '#dca5ff'
-};
-//linear-gradient(to bottom, #68Eacc 0%, #497BE8 100%);
-//background: linear-gradient(to bottom,#285c50 0%,#111d36 100%);
 const themeRedux = (state = lightTheme, {type}) => {
     switch (type) {
         case SWITCH_LIGHT_THEME:
