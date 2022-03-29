473c23c62d9196df4f2dbcd7b83dc18e88644be3
shapikso <79461623+shapikso@users.noreply.github.com>
2022-03-29 16:43:02 +0300

    done HW with tests

--- src/store/sagas.ts
+++ src/store/sagas.ts
@@ -1,7 +1,7 @@
 import {all, spawn} from 'redux-saga/effects';
-import todosSagas from './todos/saga/index';
+import {todosSaga} from './todos/saga';
 
 export default function* rootSaga() {
-    const sagas = [todosSagas];
+    const sagas = [todosSaga];
     yield all(sagas.map(s => spawn(s)));
 }
--- src/store/types.ts
+++ src/store/types.ts
@@ -1,13 +1,3 @@
-// import {TTodos} from "./todos/types";
-// import {TTheme} from "./theme/types";
-// import {TNotify} from "./notify/types";
 import { rootReducer } from "./index";
 
-// eslint-disable-next-line no-undef
 export type ApplicationState = ReturnType<typeof rootReducer>;
-
-// export type TStore = {
-//     todos: TTodos[],
-//     theme: TTheme,
-//     notification: TNotify
-// };
