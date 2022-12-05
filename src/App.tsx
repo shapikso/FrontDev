01c989158f2970f4262a79a9fe4d0aa664dac391
shapikso <shapovalov.lit@gmail.com>
2022-12-05 16:47:39 +0200

    done final fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import React, {useEffect} from 'react';
 import { Routes, Route} from "react-router-dom";
-import {useDispatch} from "react-redux";
-import {loadCitiesList} from "./store/weather/thunks";
 import WeatherList from "./components/WeatherList/WeatherList";
 import SingleWeather from "./components/SingleWeather/SingleWeather";
 
--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import {Provider} from 'react-redux';
 import {store} from './store';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { GlobalStyle } from './GlobalStyles';
 ReactDOM.render(
-    <BrowserRouter>
+    <HashRouter>
         <Provider store={store}>
             <GlobalStyle/>
             <App />
         </Provider>
-    </BrowserRouter>,
+    </HashRouter>,
     document.getElementById('root')
 );
 
